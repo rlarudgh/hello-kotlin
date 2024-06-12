9b2be89557428bec29f2dfddf17aa75a3ad716fa
rlarudgh <nestjs01@naver.com>
2024-06-12 12:20:09 +0900

    import & package

---
+++ 2.2/geometry/shapes/pkg.kt
@@ -0,0 +1,16 @@
+package geometry.shapes
+
+import java.util.Random
+
+class Rectangle (val height: Int, val width: Int) {
+    val isSquare: Boolean
+        get() {
+            return height == width
+        }
+}
+
+fun createRandomRectangle(): Rectangle {
+    val random = Random()
+    
+    return Rectangle(random.nextInt(), random.nextInt())
+}
